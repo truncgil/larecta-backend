24011c7b4cf0f60ed80bc4d2f3a55c1ebae9d479
Ümit Tunç <umitreva@gmail.com>
2025-01-10 18:46:14 +0300

    Integrate HasRoles trait into User model for enhanced role management

--- app/Models/User.php
+++ app/Models/User.php
@@ -4,11 +4,12 @@ use Laravel\Sanctum\HasApiTokens;
 use Illuminate\Notifications\Notifiable;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
+use App\Traits\HasRoles;
 
 
 class User extends Authenticatable
 {
-    use HasApiTokens, HasFactory, Notifiable;
+    use HasApiTokens, HasFactory, Notifiable, HasRoles;
 
     protected $fillable = [
         'name',
