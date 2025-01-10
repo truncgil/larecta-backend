c3b497814af07240d0435d2219c45b06208080f8
Ümit Tunç <umitreva@gmail.com>
2025-01-10 22:09:02 +0300

    Add API routes for managing permissions, roles, and user roles

--- routes/api.php
+++ routes/api.php
@@ -1,6 +1,9 @@
 <?php
 
 use App\Http\Controllers\Api\AuthController;
+use App\Http\Controllers\Api\PermissionController;
+use App\Http\Controllers\Api\RoleController;
+use App\Http\Controllers\Api\UserRoleController;
 use Illuminate\Support\Facades\Route;
 
 Route::prefix('auth')->group(function () {
@@ -13,5 +16,19 @@ Route::prefix('auth')->group(function () {
         Route::post('/logout', [AuthController::class, 'logout']);
         Route::get('/user', [AuthController::class, 'user']);
         Route::put('/profile', [AuthController::class, 'updateProfile']);
+    });
+}); 
+
+// Permission, Role ve User Role routes - sadece super-admin erişebilir
+Route::middleware(['auth:sanctum', 'super-admin'])->group(function () {
+    Route::apiResource('permissions', PermissionController::class);
+    Route::apiResource('roles', RoleController::class);
+    
+    Route::prefix('user-roles')->group(function () {
+        Route::get('/', [UserRoleController::class, 'index']);
+        Route::post('/', [UserRoleController::class, 'store']);
+        Route::get('/{user}', [UserRoleController::class, 'show']);
+        Route::put('/{user}', [UserRoleController::class, 'update']);
+        Route::delete('/{user}', [UserRoleController::class, 'destroy']);
     });
 }); 
