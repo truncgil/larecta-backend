cc9168ca0d856be9f50bb6c66bc9e38f5432f875
Ümit Tunç <umitreva@gmail.com>
2025-01-15 19:44:35 +0300

    Add migration to update 'content' column type in 'contents' table

---
+++ database/migrations/2025_01_15_131845_update_content_column_in_contents_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class UpdateContentColumnInContentsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('contents', function (Blueprint $table) {
+            // 'content' sütununu LONGTEXT olarak değiştir
+            $table->longText('content')->change();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('contents', function (Blueprint $table) {
+            // 'content' sütununu eski haline döndür (eğer eski hali TEXT ise)
+            $table->text('content')->change();
+        });
+    }
+}
